fe8d45dd1ea811619fa0bd40223848b66dc1074e
Gabriel Genest <9.genest.gabriel@gmail.com>
2022-03-06 18:39:34 -0500

    plus de code

--- source/fileReader.py
+++ source/fileReader.py
@@ -15,9 +15,10 @@ class FileReader:
 		self.parent_directories = split[:-1]
 		self.filename = split[-1]
 		self.date, self.fullname = self.filename.split(name_metadata_sep)
-		self.date = datetime.date(*self.date.split("-"))
+		self.date = datetime.date(*map(int, self.date.split("-")))
 		self.name, self.extension = self.fullname.split(".")
-		self.metadata = {"Nom": self.name, "Date": self.date, "Dossier": tuple(self.parent_directories)}
+		self.metadata = {"Nom": self.name, "Date": self.date, "Dossier": tuple(self.parent_directories),
+						 "Type": self.extension, "Header": None, "Colonnes": None}
 		self.header_lines = header_lines
 
 	def read_file(self):
@@ -27,18 +28,33 @@ class FileReader:
 			return self._read_txt()
 
 	def _read_csv(self):
-		data = pd.read_csv(self.filepath, header=self.header_lines)
-		data = data.to_numpy()
-		return data
+		data = []
+		with open(self.filepath, "r") as file:
+			lines = file.readlines()
+		header = list(map(lambda x: x.strip(), lines[:self.header_lines]))
+		colonnes = lines[self.header_lines + 1].strip().split(",")
+		self.metadata["Colonnes"] = tuple(colonnes)
+		self.metadata["Header"] = tuple(header)
+		for line in lines[self.header_lines + 1:]:
+			data.append(list(map(float, line.split(","))))
+		return np.array(data)
 
 	def _read_txt(self):
 		data = []
 		with open(self.filepath, "r") as file:
 			lines = file.readlines()
-		header = lines[:self.header_lines]
-		for line in lines[self.header_lines:]:
+		header = list(map(lambda x: x.strip(), lines[:self.header_lines]))
+		colonnes = lines[self.header_lines + 1].strip().split(" ")
+		self.metadata["Colonnes"] = tuple(colonnes)
+		self.metadata["Header"] = tuple(header)
+		for line in lines[self.header_lines + 1:]:
 			data.append(list(map(float, line.split(" "))))
-		return np.array(data), header
+		return np.array(data),
+
 
 if __name__ == '__main__':
-	f = r"test\test.txt"
+	f = r"2022-03-06_test.csv"
+	fr = FileReader(f)
+	print(fr.metadata)
+	print(fr.read_file())
+	print(fr.metadata)
