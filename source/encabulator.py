51dc7bc072f60108001c4eea6d8338bc523bcaa1
Gabriel Genest <9.genest.gabriel@gmail.com>
2022-03-06 20:44:19 -0500

    plotter presque fini

--- source/encabulator.py
+++ source/encabulator.py
@@ -1,8 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import typing
 import os
 
 
-class EncabulatorReader:
+class EncabulatorData:
 
 	def __init__(self, filepath: str, name_metadata_sep: str = "_", header_lines: int = 2):
 		self.filepath = filepath
@@ -16,7 +18,8 @@ class EncabulatorReader:
 		self.date, self.fullname = self.filename.split(name_metadata_sep)
 		self.name, self.extension = self.fullname.split(".")
 		self.metadata = {"Nom": self.name, "Date": self.date, "Dossier": tuple(self.parent_directories),
-						 "Type": self.extension, "Header": ("Fichier non lu",), "Colonnes": ("Fichier non lu",)}
+						 "Type": self.extension, "Header": ("Fichier non lu",), "Colonnes": ("Fichier non lu",),
+						 "Apparatus": "Fichier non lu"}
 		self.header_lines = header_lines
 		self.data = None
 
@@ -34,6 +37,7 @@ class EncabulatorReader:
 		colonnes = lines[self.header_lines].strip().split(",")
 		self.metadata["Colonnes"] = tuple(colonnes)
 		self.metadata["Header"] = tuple(header)
+		self.metadata["Apparatus"] = header[-1].split(" = ")[-1]
 		for line in lines[self.header_lines + 1:]:
 			data.append(list(map(float, line.split(","))))
 		self.data = np.array(data)
@@ -47,17 +51,26 @@ class EncabulatorReader:
 		colonnes = lines[self.header_lines].strip().split(" ")
 		self.metadata["Colonnes"] = tuple(colonnes)
 		self.metadata["Header"] = tuple(header)
+		self.metadata["Apparatus"] = header[-1].split(" = ")[-1]
 		for line in lines[self.header_lines + 1:]:
 			data.append(list(map(float, line.split(" "))))
 		self.data = np.array(data)
 		return self.data
+
+	@property
+	def time(self):
+		return self.data[:, 0]
+
+	@property
+	def encabulator_data(self):
+		return self.data[:, 1]
 
 	def __str__(self):
 		msg = f"EncabulatorReader de `{self.filename}`"
 		return msg
 
 
-class MultipleEncabulatorReader:
+class EncabulatorDataset:
 
 	def __init__(self, dir: str, name_metadata_sep: str = "_", header_lines: int = 2):
 		self.dir = dir
@@ -71,9 +84,10 @@ class MultipleEncabulatorReader:
 		for file in all_files:
 			if file in exceptions:
 				continue
-			er = EncabulatorReader(os.path.join(self.dir, file), self.name_metadata_sep, self.header_lines)
+			er = EncabulatorData(os.path.join(self.dir, file), self.name_metadata_sep, self.header_lines)
 			er.read_file()
-			ers = encabulator_readers.setdefault(er.date, set())
+			apparatus = er.metadata["Apparatus"]
+			ers = encabulator_readers.setdefault(apparatus, set())
 			ers.add(er)
 		self.encabulator_readers = encabulator_readers
 		return self.encabulator_readers
@@ -84,9 +98,46 @@ class MultipleEncabulatorReader:
 		return str(self.encabulator_readers)
 
 
+class EncabulatorPlotter:
+
+	def __init__(self, encabulator_reader: typing.Union[EncabulatorData, EncabulatorDataset], exceptions: tuple = ()):
+		if isinstance(encabulator_reader, EncabulatorData):
+			encabulator_reader.read_file()
+			data = {encabulator_reader.metadata["Apparatus"]: encabulator_reader}
+		elif isinstance(encabulator_reader, EncabulatorDataset):
+			encabulator_reader.read_all(exceptions)
+			data = encabulator_reader.encabulator_readers
+		else:
+			raise TypeError(f"Type `{type(encabulator_reader)}` n'est pas supporté.")
+		self.data = data
+
+	def plot(self):
+		# TODO save
+		fig, axes = plt.subplots(len(self.data))
+		axes = np.ravel(axes)
+		count = 0
+		for apparatus, readers in self.data.items():
+			for reader in readers:
+				axes[count].plot(reader.time, reader.encabulator_data, label=reader.date)
+			count += 1
+		plt.show()
+
+
+	def plot_avg(self, ax: plt.Axes = None):
+		# TODO save
+		pass
+
+
+class EncabulatorStats:
+
+	def __init__(self, encabulator_reader: typing.Union[EncabulatorData, EncabulatorDataset]):
+		pass
+
+
 if __name__ == '__main__':
 	path = r"C:\Users\goubi\Documents\GitHub\progFest_OOPUnitTests\data"
 	dir = "../data"
-	ers = MultipleEncabulatorReader(dir)
+	ers = EncabulatorDataset(dir)
 	ers.read_all()
-	print(ers)
+	ep = EncabulatorPlotter(ers)
+	ep.plot()
