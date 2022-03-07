1f6be96b0b219618e2862f6a5d70da877f1ef042
Gabriel Genest <9.genest.gabriel@gmail.com>
2022-03-06 21:11:46 -0500

    fixed some stuff

--- source/encabulator.py
+++ source/encabulator.py
@@ -6,7 +6,7 @@ import os
 
 class EncabulatorData:
 
-	def __init__(self, filepath: str, name_metadata_sep: str = "_", header_lines: int = 2):
+	def __init__(self, filepath: str):
 		self.filepath = filepath
 		if "\\" in self.filepath:
 			dir_sep = "\\"
@@ -15,12 +15,11 @@ class EncabulatorData:
 		split = self.filepath.split(dir_sep)
 		self.parent_directories = split[:-1]
 		self.filename = split[-1]
-		self.date, self.fullname = self.filename.split(name_metadata_sep)
+		self.date, self.fullname = self.filename.split("_")
 		self.name, self.extension = self.fullname.split(".")
 		self.metadata = {"Nom": self.name, "Date": self.date, "Dossier": tuple(self.parent_directories),
 						 "Type": self.extension, "Header": ("Fichier non lu",), "Colonnes": ("Fichier non lu",),
 						 "Apparatus": "Fichier non lu"}
-		self.header_lines = header_lines
 		self.data = None
 
 	def read_file(self):
@@ -33,12 +32,12 @@ class EncabulatorData:
 		data = []
 		with open(self.filepath, "r") as file:
 			lines = file.readlines()
-		header = list(map(lambda x: x.strip(), lines[:self.header_lines]))
-		colonnes = lines[self.header_lines].strip().split(",")
+		header = list(map(lambda x: x.strip(), lines[:2]))
+		colonnes = lines[2].strip().split(",")
 		self.metadata["Colonnes"] = tuple(colonnes)
 		self.metadata["Header"] = tuple(header)
 		self.metadata["Apparatus"] = header[-1].split(" = ")[-1]
-		for line in lines[self.header_lines + 1:]:
+		for line in lines[3:]:
 			data.append(list(map(float, line.split(","))))
 		self.data = np.array(data)
 		return self.data
@@ -47,12 +46,12 @@ class EncabulatorData:
 		data = []
 		with open(self.filepath, "r") as file:
 			lines = file.readlines()
-		header = list(map(lambda x: x.strip(), lines[:self.header_lines]))
-		colonnes = lines[self.header_lines].strip().split(" ")
+		header = list(map(lambda x: x.strip(), lines[:2]))
+		colonnes = lines[2].strip().split(" ")
 		self.metadata["Colonnes"] = tuple(colonnes)
 		self.metadata["Header"] = tuple(header)
 		self.metadata["Apparatus"] = header[-1].split(" = ")[-1]
-		for line in lines[self.header_lines + 1:]:
+		for line in lines[3:]:
 			data.append(list(map(float, line.split(" "))))
 		self.data = np.array(data)
 		return self.data
@@ -66,16 +65,14 @@ class EncabulatorData:
 		return self.data[:, 1]
 
 	def __str__(self):
-		msg = f"EncabulatorReader de `{self.filename}`"
+		msg = f"EncabulatorData de `{self.filename}`"
 		return msg
 
 
 class EncabulatorDataset:
 
-	def __init__(self, dir: str, name_metadata_sep: str = "_", header_lines: int = 2):
+	def __init__(self, dir: str):
 		self.dir = dir
-		self.name_metadata_sep = name_metadata_sep
-		self.header_lines = header_lines
 		self.encabulator_readers = None
 
 	def read_all(self, exceptions: tuple = ()):
@@ -84,7 +81,7 @@ class EncabulatorDataset:
 		for file in all_files:
 			if file in exceptions:
 				continue
-			er = EncabulatorData(os.path.join(self.dir, file), self.name_metadata_sep, self.header_lines)
+			er = EncabulatorData(os.path.join(self.dir, file))
 			er.read_file()
 			apparatus = er.metadata["Apparatus"]
 			ers = encabulator_readers.setdefault(apparatus, set())
@@ -94,7 +91,7 @@ class EncabulatorDataset:
 
 	def __str__(self):
 		if self.encabulator_readers is None:
-			return f"Ce MultipleEncabulatorReader est vide ({repr(self)}))"
+			return f"Ce EncabulatorDataset est vide ({repr(self)}))"
 		return str(self.encabulator_readers)
 
 
@@ -111,14 +108,16 @@ class EncabulatorPlotter:
 			raise TypeError(f"Type `{type(encabulator_reader)}` n'est pas supporté.")
 		self.data = data
 
-	def plot(self, savename: str = None, show: bool = True):
-		# TODO save
-		fig, axes = plt.subplots(len(self.data))
+	def plot(self, save_name: str = None, show: bool = True):
+		fig, axes = plt.subplots(1, len(self.data))
 		axes = np.ravel(axes)
 		count = 0
 		for apparatus, readers in self.data.items():
+			if isinstance(readers, EncabulatorData):
+				readers = [readers]
+			print(readers)
 			for reader in readers:
-				axes[count].plot(reader.time, reader.encabulator_data, label=reader.date)
+				axes[count].scatter(reader.time, reader.encabulator_data, label=reader.date, alpha=0.2)
 			axes[count].legend()
 			axes[count].set_xlabel("Temps [ns]")
 			axes[count].set_ylabel("Encabulator []")
@@ -126,24 +125,55 @@ class EncabulatorPlotter:
 			count += 1
 		if show:
 			plt.show()
-		if savename is not None:
-			plt.savefig(savename)
+		if save_name is not None:
+			plt.savefig(save_name)
 
-	def plot_avg(self, ax: plt.Axes = None):
+	def plot_avg(self, save_name: str = None, show: bool = True):
 		# TODO save
 		pass
 
 
 class EncabulatorStats:
 
-	def __init__(self, encabulator_reader: typing.Union[EncabulatorData, EncabulatorDataset]):
-		pass
+	def __init__(self, encabulator_readers: EncabulatorDataset, exceptions: tuple = ()):
+		encabulator_readers.read_all(exceptions)
+		self.data = encabulator_readers.encabulator_readers
+
+	def average(self):
+		averages = []
+		for apparatus, readers in self.data.items():
+			y_s = []
+			for reader in readers:
+				y_s.append(reader.encabulator_data)
+			averages.append(np.mean(y_s, 0))
+		return averages
+
+	def median(self):
+		medians = []
+		for apparatus, readers in self.data.items():
+			y_s = []
+			for reader in readers:
+				y_s.append(reader.encabulator_data)
+			medians.append(np.median(y_s, 0))
+		return medians
+
+	def std_dev(self):
+		std_devs = []
+		for apparatus, readers in self.data.items():
+			y_s = []
+			for reader in readers:
+				y_s.append(reader.encabulator_data)
+			std_devs.append(np.std(y_s, 0))
+		return std_devs
 
 
 if __name__ == '__main__':
-	path = r"C:\Users\goubi\Documents\GitHub\progFest_OOPUnitTests\data"
 	dir = "../data"
 	ers = EncabulatorDataset(dir)
 	ers.read_all()
 	ep = EncabulatorPlotter(ers)
+	ep.plot()
+	path = "../data/2022-02-30_experiment1.txt"
+	er = EncabulatorData(path)
+	ep =EncabulatorPlotter(er)
 	ep.plot()
